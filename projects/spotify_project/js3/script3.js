07deaff9525e0e8c4263394f956e9a14906fc6c4
yuva-ind <108192389+yuva-ind@users.noreply.github.com>
2024-05-25 22:23:30 +0530

    Update script3.js

--- projects/spotify_project/js3/script3.js
+++ projects/spotify_project/js3/script3.js
@@ -20,6 +20,7 @@ function secondsToMinutesSeconds(seconds) {
 async function getSongs(folder) {
     currFolder = folder;
     let a = await fetch(`/Portfolio/projects/spotify_project/${folder}/`)
+    console.log("@@@@@@", a);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -79,6 +80,7 @@ const playMusic = (track, pause = false) => {
 async function displayAlbums() {
     console.log("displaying albums")
     let a = await fetch(`/Portfolio/projects/spotify_project/songs/`)
+    console.log("######", a);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -91,6 +93,7 @@ async function displayAlbums() {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
             let a = await fetch(`/Portfolio/projects/spotify_project/songs/${folder}/info.json`)
+            console.log("$$$$$", a);
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
@@ -113,6 +116,7 @@ async function displayAlbums() {
         e.addEventListener("click", async item => {
             console.log("Fetching Songs")
             songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)  
+            console.log("*******", songs);
             playMusic(songs[0])
 
         })
