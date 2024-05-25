cb273483e73de8eeea4712e3e43447c637352999
yuva-ind <108192389+yuva-ind@users.noreply.github.com>
2024-05-25 20:36:56 +0530

    Update script3.js

--- projects/spotify_project/js3/script3.js
+++ projects/spotify_project/js3/script3.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/Portfolio/projects/spotify_project/${folder}/`)
+    let a = await fetch(`https://github.com/yuva-ind/Portfolio/projects/spotify_project/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -61,7 +61,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/Portfolio/projects/spotify_project/${currFolder}/` + track
+    currentSong.src = `https://github.com/yuva-ind/Portfolio/projects/spotify_project/${currFolder}/` + track
     if (!pause) {
         currentSong.play()
         play.src = "img/pause.svg"
@@ -78,7 +78,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/Portfolio/projects/spotify_project/songs/`)
+    let a = await fetch(`https://github.com/yuva-ind/Portfolio/projects/spotify_project/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -90,7 +90,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`/Portfolio/projects/spotify_project/songs/${folder}/info.json`)
+            let a = await fetch(`https://github.com/yuva-ind/Portfolio/projects/spotify_project/songs/${folder}/info.json`)
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
@@ -101,7 +101,7 @@ async function displayAlbums() {
                 </svg>
             </div>
 
-            <img src="/Portfolio/projects/spotify_project/songs/${folder}/cover.jpg" alt="">
+            <img src="https://github.com/yuva-ind/Portfolio/projects/spotify_project/songs/${folder}/cover.jpg" alt="">
             <h2>${response.title}</h2>
             <p>${response.description}</p>
         </div>`
